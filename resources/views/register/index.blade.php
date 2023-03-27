47c41a2fa8a043e1c74bb6a65677efbdc1ca0196
Muhammad Ainul Yaqin <myaqien27@gmail.com>
2023-03-28 00:31:52 +0700

    user registration

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -3,25 +3,59 @@
 @section('container')
     <div class="row justify-content-center">
         <div class="col-lg-5">
-
+            
             <main class="form-registration w-100 m-auto">
                 <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
-                <form>
+                <form action="/register" method="POST">
+                    @csrf
                     <div class="form-floating">
-                        <input type="text" name="name" class="rounded-top form-control" id="name">
+                        <input type="text" name="name" class="rounded-top form-control name" required id="name"
+                            value="{{ old('name') }}">
                         <label for="name">Name</label>
+                        @error('name')
+                            <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                               {{ $message  }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="text" name="username" class="form-control" id="username">
+                        <input type="text" name="username" value="{{ old('username') }}"
+                            class="form-control @error('username')
+                        is-invalid
+                        @enderror"
+                            required id="username">
                         <label for="username">userName</label>
+                        @error('username')
+                            <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                {{ $message  }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                        <input type="email" name="email"
+                            class="form-control @error('email')
+                        is-invalid
+                        @enderror"
+                            required value="{{ old('email') }}" id="email" placeholder="name@example.com">
                         <label for="floatingInput">Email address</label>
+                        @error('email')
+                            <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                {{ $message  }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="rounded-bottom form-control" id="password" placeholder="Password">
+                        <input type="password" name="password"
+                            class="rounded-bottom form-control @error('password')
+                        is-invalid
+                        @enderror"
+                            required id="password" placeholder="Password">
                         <label for="floatingPassword">Password</label>
+                        @error('password')
+                            <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                {{ $message  }}
+                            </div>
+                        @enderror
                     </div>
 
                     <button class="w-100 btn btn-lg btn-danger mt-3" type="submit">Register</button>
--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,7 @@ Route::get("/categories", function (Category $category) {
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
 
 //Programmer Zaman Now
